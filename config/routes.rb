92f3da2e6e00fcc313edaa664d5adac7c58ee8f3
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2019-06-30 16:49:25 +0530

    dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
   devise_for :artists
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
