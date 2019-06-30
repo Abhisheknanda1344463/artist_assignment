6bd4e86ed5061a92964b06c068499eb69f2e50a9
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2019-06-30 19:07:26 +0530

    removed dashboard, added artists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-
   devise_for :artists
-  root 'dashboard#index'
+  resources :artists, only: [:show, :index]
+  root 'artists#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
